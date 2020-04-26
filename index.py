c2ec24b22d3174b250e5ec619f405f763c945963
gastbob40 <gastbob40@gmail.com>
2020-04-26 12:45:37 +0200

    feat (config): remove config file and add default config file

--- index.py
+++ index.py
@@ -2,14 +2,6 @@ import discord
 import yaml
 
 from src.on_ready.on_ready import on_ready_handler
-from src.utils.api_manager import APIManager
-
-sanctions_manager = APIManager()
-
-# An example
-# a = sanctions_manager.get_data('news-groups')[1][1]
-# sanctions_manager.edit_data('news-groups', a['id'], name="testresdsfs")
-# print(a)
 
 with open('run/config/tokens.yml', 'r') as file:
     tokens = yaml.safe_load(file)
